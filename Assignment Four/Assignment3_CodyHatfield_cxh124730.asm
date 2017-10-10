6bc726be18016d7100602290a9fc7b6c38c13ef3
hatfield-c <chatfield@ymail.com>
2017-10-09 20:21:23 -0500

    [AssnFour-1] Initial commit

---
+++ Assignment Four/Assignment3_CodyHatfield_cxh124730.asm
@@ -0,0 +1,13 @@
+# A program that gets a zip code input
+# from the user, stores it in memory,
+# and sums up its digits via two different
+# functions.
+
+.data
+	
+	str_wlcm:		.asciiz 	"Welcome! This program will get a zip code from you, and outputs the sum of its digits.\nPlease enter a zip code: "
+
+.text main:
+
+	
+exit:
