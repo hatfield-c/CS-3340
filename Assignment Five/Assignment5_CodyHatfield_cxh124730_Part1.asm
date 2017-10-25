b6fe3deebf1d31ef63dfcfe0109a32bd5e433e0f
hatfield-c <chatfield@ymail.com>
2017-10-24 19:39:23 -0500

    [AssnFive-3] Create the main menu subroutine for part one

--- Assignment Five/Assignment5_CodyHatfield_cxh124730_Part1.asm
+++ Assignment Five/Assignment5_CodyHatfield_cxh124730_Part1.asm
@@ -9,7 +9,8 @@
 	str_mainMenuOptions:	.asciiz		"1. Set Conversion Rate\n2. Convert USD to Yen\n3. Convert Yen to USD\n4. Exit Application\n"
 	str_newLine:		.asciiz		"\n"
 
-.text main:
+.text 
+main:
 
 	#method: Clear the command line screen
 	jal clearScreen
@@ -18,13 +19,65 @@
 	jal getMainMenu
 	move $t0, $v0
 	
+	#conditional: Branch to correct functionality based on input. If input invalid, restart application.
+	beq $t0, 1, O1
+	beq $t0, 2, O2
+	beq $t0, 3, O3
+	beq $t0, 4, terminate_application
+	j main
+	
+	#Option 1: Set conversion rate
+	O1:
+		jal setRate
+		j end_switch
+	#Option 2: Convert Yen to USD
+	O2:
+		jal yenToUsd
+		j end_switch
+	#Option 1: Convert USD to Yen
+	O3:
+		jal usdToYen
+		j end_switch
+	
+	end_switch:
+	#method: Restart application
+	j main
+	
+	#method: End the application
 	terminate_application:
 	
 	li $v0, 10
 	syscall
 	
-getMainMenu:
+setRate:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	jr $ra
+	
+yenToUsd:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+
+	jr $ra
+	
+usdToYen:
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	jr $ra
+
+#subroutine: Displays the main menu, and returns the user input selected from the main menu
+getMainMenu:
 	#output: Print the main menu title
 	la $a0, str_mainMenu
 	li $v0, 4
@@ -40,14 +93,16 @@ getMainMenu:
 	syscall
 	
 	jr $ra
-	
+
+#subroutine: Prints a newline character
 printNewLine:
 	#method: prints a newline into the console
 	la $a0, str_newLine
 	li $v0, 4
 	syscall
 	jr $ra
-	
+
+#subroutine: Clears the command line screen	
 clearScreen:
 	#method: clears the screen by printing several new lines into the console
 	addi $sp, $sp, -4
